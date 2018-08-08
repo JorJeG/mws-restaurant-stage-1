83b623ac916f698c86b807d2a921e105a2adf7ad
Georgiy Starkov <gstarkov658@gmail.com>
2018-08-08 21:57:08 +0300

    Add aria

--- js/main.js
+++ js/main.js
@@ -180,6 +180,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('role-label', `${restaurant.name} details`);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
