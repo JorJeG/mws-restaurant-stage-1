43cb6b9f050dd7123c935064bca0a504ed94c10b
Georgiy Starkov <gstarkov658@gmail.com>
2018-08-09 16:41:18 +0300

    Fix typo

--- js/main.js
+++ js/main.js
@@ -171,7 +171,7 @@ const createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
-  more.setAttribute('role-label', `${restaurant.name} details`);
+  more.setAttribute('aria-label', `${restaurant.name} details`);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
